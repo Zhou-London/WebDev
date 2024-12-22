8c511700943cf3949346de2583b86b5c57a63cd5
zhou <zhou@zhous-MacBook-Pro.local>
2024-12-22 22:54:20 +0000

    Django Update

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -12,4 +12,6 @@ def William(request):
     return HttpResponse("Hello William!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
